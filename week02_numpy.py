0a59a7121a702fc1da178ac63a84af77a918eddb
DonghaeK <rlaehdgo5598@naver.com>
2023-09-07 16:24:12 +0900

    week02 v0.6

--- week02_numpy.py
+++ week02_numpy.py
@@ -2,12 +2,11 @@ import numpy as np
 import random
 
 x = int(input("수를 입력하세요 : "))
-l = list()
-for i in range(x):
-    l.append(random.randint(1, 100))
 
-    v = np.array(l, dtype='int16')
-    print(v)
+l = [random.randint(1, 100) for i in range(x)]
+
+v = np.array(l, dtype='int16')
+print(v)
 
 
 
