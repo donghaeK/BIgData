890a8628754b192c3c518a4c9a67dcec593eea4b
DonghaeK <rlaehdgo5598@naver.com>
2023-09-14 15:54:37 +0900

    week03_numpy v1.6

--- week03_numpy.py
+++ week03_numpy.py
@@ -1,24 +1,13 @@
 import numpy as np
-# import random
 import tkinter as tk  # Built in GUI
 from tkinter import messagebox  # pop-up window
 
-def create_2darray(row, col):
-    return np.random.randint(1, 101, size=(row, col))
-
-
-
-
-def click_button():
+def click_button(ev):
     try:
         r, c = map(int, en_row_column.get().split())  # spacebar
-
-        # rows = [[random.randint(1, 100) for i in range(r)] for i in range(c)]
-        # matrix = np.array(create_2darray(r,c), dtype='int16')
-        matrix = create_2darray(r, c)
+        matrix = np.random.randint(1, 101, size=(r, c))
         lbl_result.config(text=matrix)
     except ValueError as err:
-        lbl_result.config(text=f"입력 값이 없습니다.\n{err}")
         messagebox.showerror('Error!', f"입력 값이 없습니다.\n{err}")
 
 
@@ -29,16 +18,12 @@ window.geometry('300x150')
 # create widget
 lbl_result = tk.Label(text="random numpy 2d array")
 en_row_column = tk.Entry()
-#en_column = tk.Entry()
 btn_click = tk.Button(text="click me!", command=click_button)
 
-# widget layout
-# lbl_result.grid(row=0, column=0, columnspan=2)
-# en_row.grid(row=1, column=0)
-# en_column.grid(row=1, column=1)
-# btn_click.grid(row=2, column=0, columnspan=2, sticky=tk.EW)
 lbl_result.pack()
 en_row_column.pack(fill='x')
 btn_click.pack(fill='x')
+
+en_row_column.bind("<Return>", click_button)
 
 window.mainloop()
