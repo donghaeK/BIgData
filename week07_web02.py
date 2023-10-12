16d3d33b84c9ae1e8911f4ec633ae8adc09ff33b
DonghaeK <rlaehdgo5598@naver.com>
2023-10-12 15:26:36 +0900

    week07 v3.4

--- week07_web02.py
+++ week07_web02.py
@@ -6,6 +6,8 @@ html = """
 <title>스크레이핑 실습</title>
 </head>
 <body>
+<a href="http:///www.daelim.ac.kr">대림대학교</a><br>
+<a href="http:///www.harvard.edu">하버드대학교</a>
 <h1>daelimUniversity</h1>
 <p>웹 스크레이핑</p>
 <p>파이썬 기본 문법, 넘파이, 판다스, 맷플롯립, 사이킷런, GUI ... </p>
@@ -14,8 +16,11 @@ html = """
 """
 
 soup = BeautifulSoup(html, 'html.parser')
-t = soup.html.head.title.string
-h1 = soup.html.body.h1.string
-p1 = soup.html.body.p
-p2 = p1.next_sibling.next_sibling
-print(t, h1, p1, p2)
+urls = soup.find_all("a")
+
+for url in urls:
+    print(f'{url.string}의 url주소는 {url.attrs["href"]}입니다!')
+    # univ = url.string
+    # link = url.attrs['href']
+    # print(univ)
+    # print(link)
