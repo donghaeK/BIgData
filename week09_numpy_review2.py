b9b13a13fd9f54e85b58b6943b7f12a47a835333
DonghaeK <rlaehdgo5598@naver.com>
2023-10-26 15:59:13 +0900

    week09_numpy_review ver1.3

--- week09_numpy_review2.py
+++ week09_numpy_review2.py
@@ -1,23 +1,16 @@
 import numpy as np
-import pandas as pd
-df = pd.DataFrame({
-    'A' : [1, 2, np.nan, 4],
-    'B' : [5, np.nan, 7, 8],
-    'C' : [9, 10, 11, np.nan]
-})
+data = np.array([
+     [1, 5, 9],
+     [2, np.nan, 10],
+     [np.nan, 7, 11],
+     [4, 8, np.nan]
+])
+print(data)
 
-#1
-#df.fillna(df.mean(), inplace=True)
-#print(df)
+means= np.nanmean(data, axis=0)
+print(means)
 
-#2
-# for col in df.columns:
-#     print(col)
-#     df[col] = np.where(pd.isnull(df[col]), np.mean(df[col]), df[col])
-# print(df)
-
-#3
-from sklearn.impute import SimpleImputer
-i = SimpleImputer(strategy='mean')
-df = pd.DataFrame(i.fit_transform(df),columns=df.columns)
-print(df)
+for i in range(data.shape[1]):
+    mask = np.isnan(data[:, i]) #nan인부분 true / false로 구분
+    data[mask, i] = means[i]
+    print(data)
