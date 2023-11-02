ffa8c0823601dcfa6077cc4de1db042401ccd71f
DonghaeK <rlaehdgo5598@naver.com>
2023-11-02 15:25:29 +0900

    week10 numpy_review v0.2

--- week10_numpy_review.py
+++ week10_numpy_review.py
@@ -1,12 +1,22 @@
 import numpy as np
-a1 = np.array([
-    [1,2],
-    [3,2]
-])
-a2 = np.array([
-    [2, 1],
-    [5, 0]
-])
-#print(np.dot(a1, a2))
-print(a1@a2)
-print(np.matmul(a1,a2))
+# a1 = np.array([
+#     [1,1,2],
+#     [1,1,2],
+#     [1,1,2]
+# ])
+# a2 = np.array([
+#     [1,0,0],
+#     [2,0,0],
+#     [1,0,0]
+# ])
+a1 = np.ones((3,3))
+a1[:, 2] =2
+print(a1)
+
+
+a2 = np.full((3,3), 5)
+print(a2)
+
+a3 = np.eye(3) #단일 행렬
+print(a3)
+#print(np.matmul(a1,a2))
