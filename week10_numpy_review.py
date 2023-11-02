36b08f8e21340aa3b25a36eb579ef01de14bc0ce
DonghaeK <rlaehdgo5598@naver.com>
2023-11-02 15:37:15 +0900

    week10 numpy_review v0.3

--- week10_numpy_review.py
+++ week10_numpy_review.py
@@ -13,10 +13,22 @@ a1 = np.ones((3,3))
 a1[:, 2] =2
 print(a1)
 
-
 a2 = np.full((3,3), 5)
 print(a2)
 
 a3 = np.eye(3) #단일 행렬
 print(a3)
-#print(np.matmul(a1,a2))
+
+a4 = a3[:2, 1:]
+print(a4)
+
+a5 = a3[2]
+#a5 = a3[2:, :] #(1, 3)
+print(a5)
+
+a6 = a3[1, :2]
+#a6 = a3[1:2, :2]
+print(a6)
+
+a7 = a3[:, 0:2]
+print(a7)
