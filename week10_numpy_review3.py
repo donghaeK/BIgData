2fc7b580c0c47bfdab0e69d88205929deb08519c
DonghaeK <rlaehdgo5598@naver.com>
2023-11-02 16:33:46 +0900

    week10 numpy_review v0.7

--- week10_numpy_review3.py
+++ week10_numpy_review3.py
@@ -1,13 +1,12 @@
 import numpy as np
 from numpy.linalg import inv
 #linalg == linear algebra -> 역행렬함수
+import matplotlib.pyplot as plt
 np.random.seed()
-X = np.array([
-    [1,2,3],
-    [1,0,0],
-    [0,0,1]
-])
-print(X)
-Z = inv(X) #역행렬
-print(Z)
-print(X.dot(Z))
+a = np.random.uniform(size=10000) #균등분포(일양분포)
+print(a)
+b = np.random.normal(0,1,1000) #정규분포
+print(b)
+plt.hist(a)
+#plt.hist(b)
+plt.show()
