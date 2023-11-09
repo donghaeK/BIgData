8812d470e2af059e78c4027997c869ede5902d28
DonghaeK <rlaehdgo5598@naver.com>
2023-11-09 15:39:49 +0900

    week11_pandas_review v1.5

--- week11_pandas_review.py
+++ week11_pandas_review.py
@@ -9,11 +9,6 @@ df2 = pd.DataFrame(
     columns=["KOR","ENG","MAT"]
 )
 print(df2)
-#'KOR', 'MAT' column 추출 KOR>=95
-df_copy = df2.loc[df2['KOR'] >= 95, ['KOR', 'MAT']]
-print(df_copy)
-#1번학생의 100점을 뽑을때
-print(df2.at[1,'MAT'])
+#영어,국어성적이 95
 
-print(df2.iat[0,2])
-
+print(df2.query('KOR>=95 and ENG>=95'))
