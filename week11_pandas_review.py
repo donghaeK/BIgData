dc14173e4d44fa08efd4d512d9c20d5ca74dc5ad
DonghaeK <rlaehdgo5598@naver.com>
2023-11-09 16:05:04 +0900

    week11_pandas_review v1.6

--- week11_pandas_review.py
+++ week11_pandas_review.py
@@ -1,14 +1,19 @@
 import pandas as pd
 df2 = pd.DataFrame(
     [
-        [99,89,100],
+        [99,89,81],
         [91,98,98],
-        [100,97,85],
-        [83,100,94]
+        [95,97,85],
+        [83,96,94]
     ], index=[1,2,3,4],
-    columns=["KOR","ENG","MAT"]
+    columns=["KOR", "ENG", "MAT"]
 )
-print(df2)
-#영어,국어성적이 95
+#print(df2.describe())
+print(df2.mean())
 
 print(df2.query('KOR>=95 and ENG>=95'))
+
+def scale_score(n):
+    return n+1
+df2 = df2.apply(scale_score)
+print(df2)
