85c68d25121209a4aa8cef4ad0005190a99f2217
DonghaeK <rlaehdgo5598@naver.com>
2023-11-16 16:34:41 +0900

    week12 web v1.2

---
+++ week13_web05.py
@@ -0,0 +1,26 @@
+import urllib.request
+from bs4 import BeautifulSoup
+import pandas as pd
+import datetime
+
+shops = list()
+
+for i in range(1, 52):
+    url = f"https://www.hollys.co.kr/store/korea/korStore2.do?pageNo={i}&sido=&gugun=&store="
+#    print(url)
+    page = urllib.request.urlopen(url)
+    soup = BeautifulSoup(page, "html.parser")
+    tbody = soup.find('tbody')
+    trs = tbody.find_all('tr')
+    
+    for tr in trs:
+        tds = tr.find_all('td')
+        shop_name = tds[1].string #매장이름
+        shop_addr = tds[3].string  # 매장주소
+        shop_tel = tds[5].string  # 매장전화번호
+
+        shops.append([shop_name]+[shop_addr]+[shop_tel]+[datetime.datetime.now()])
+
+#print(shops)
+hollys_df = pd.DataFrame(shops, columns=('매장명', '주소', 'Tel', '일시'))
+hollys_df.to_csv('holly.csv', mode='w',encoding='cp949')
