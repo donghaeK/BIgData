7de2b36063b562574795130ed15b07bf25a13e55
DonghaeK <rlaehdgo5598@naver.com>
2023-11-16 15:26:41 +0900

    week12 web v1.1

---
+++ week12_web04.py
@@ -0,0 +1,37 @@
+from bs4 import BeautifulSoup
+
+html = """
+<html>
+<head>
+<title>스크레이핑 실습</title>
+</head>
+<body>
+<a href="http://www.daelim.ac.kr">대림대학교</a><br>
+<a href="http://www.harard.edu">하버드대학교</a><br>
+</body>
+</html>
+"""
+soup = BeautifulSoup(html,'html.parser')
+urls = soup.find_all("a")
+print(urls)
+#하버드대학교의 url주소는 http://www.harard.edu입니다.
+
+for url in urls:
+    print("{0}의 주소는 {1}입니다.".format(url.string,url.attrs['href']))
+    print(f'{url.string}의 주소는 {url.attrs["href"]}입니다.)')
+
+
+
+# <h1 id="univ">대림대학교</h1>
+# <p>웹스크레이핑</p>
+# <p id="contents">파이썬, 판다스, 넘파이, 맷플롯립, GUI...</p>
+# </body>
+# </html>
+# """
+# soup = BeautifulSoup(html, 'html.parser')
+# university = soup.find(id='univ')
+# contents = soup.find(id='contents')
+#
+# print(university)
+# print(contents.string)
+# print(contents)
