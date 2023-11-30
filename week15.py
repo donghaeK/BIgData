754b4ac9a76523ddc272ecc1f6cc4d9c164be406
DonghaeK <rlaehdgo5598@naver.com>
2023-11-30 14:30:44 +0900

    week14 v0.1

---
+++ week15.py
@@ -0,0 +1,13 @@
+import seaborn as sns
+import pandas as pd
+
+titanic = sns.load_dataset('titanic')
+#print(titanic.describe())
+#print(titanic.head(10))
+#print(titanic.tail(10))
+print(titanic['age'].fillna(titanic['age'].median(), inplace=True))
+titanic['deck'] = titanic['deck'].cat.add_categories('Unknown')  # 카테고리 추가
+titanic['deck'].fillna('Unknown', inplace=True)
+titanic['embarked'].fillna('Unknown', inplace=True)
+titanic['embark_town'].fillna('Unknown', inplace=True)
+print(titanic.info())
